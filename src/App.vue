2784447b0338edfa14431143fc866320c50eecb6
Julien SImonet <jsimonet.alaji@gmail.com>
2019-12-08 19:28:43 +0100

    add play with friends and style

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,103 @@
 <template>
   <div id="app">
     <div class="container">
+      <div class="head">
+        <button
+          class="play-bot"
+          v-on:click="hasBot = !hasBot; init()">
+          {{ hasBot ? "Jouer contre un(e) ami(e)" : "Jouer contre l'ordinateur" }}
+        </button>
+        <button>
+          <svg
+            class="symbol symbol-x"
+            aria-label="X"
+            role="img"
+            viewBox="0 0 128 128">
+            <path d="M16,16L112,112"></path>
+            <path d="M112,16L16,112"></path>
+          </svg>
+          <span class="nb-win">{{x}}</span>
+        </button>
+        <button
+          v-on:click="hasBot ? init() & playBot() : null">
+          <svg
+            class="symbol symbol-o"
+            aria-label="O"
+            role="img"
+            viewBox="0 0 128 128">
+            <path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16"></path>
+          </svg>
+          <span class="nb-win">{{o}}</span>
+        </button>
+      </div>
       <div
         v-if="showResult"
         class="result">
-        <div v-if="isWinner">
-          Gagné !
+        <div class="result-value">
+          <svg
+            v-if="!result || result === 1"
+            class="symbol symbol-x"
+            aria-label="X"
+            role="img"
+            viewBox="0 0 128 128">
+            <path d="M16,16L112,112"></path>
+            <path d="M112,16L16,112"></path>
+          </svg>
+          <svg
+            v-if="!result || result === 2"
+            class="symbol symbol-o"
+            aria-label="O"
+            role="img"
+            viewBox="0 0 128 128">
+            <path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16"></path>
+          </svg>
         </div>
-        <div v-else-if="isWinner === false">
-          Perdu !
+        <div class="result-name">
+          <span v-if="!result">Egalité !</span>
+          <span v-else>Gagné !</span>
         </div>
-        <div v-else>
-          Egalité !
-        </div>
-        <button v-on:click="init()">
-          Rejouer
-        </button>
       </div>
       <div
         v-else
-        v-for="(row, ri) in values"
-        :key="ri"
-        class="row">
-        <button
-          v-for="(col, ci) in row"
-          :key="ci"
-          v-on:click="playerPlay(ri, ci)"
-          :disabled="isDisabled(ri, ci)"
-          class="col"
-          :class="`col-${col}`">
-          {{ col }}
-        </button>
+        class="game">
+        <div
+          v-for="(row, ri) in values"
+          :key="ri"
+          class="row">
+          <button
+            v-for="(col, ci) in row"
+            :key="ci"
+            v-on:click="playerPlay(ri, ci)"
+            :disabled="isDisabled(ri, ci)"
+            class="col"
+            :class="`col-${col}`">
+            <transition name="symbol-x">
+              <svg
+                v-if="col === 'x'"
+                class="symbol symbol-x"
+                aria-label="X"
+                role="img"
+                viewBox="0 0 128 128">
+                <path d="M16,16L112,112"></path>
+                <path d="M112,16L16,112"></path>
+              </svg>
+            </transition>
+            <transition name="symbol-o">
+              <svg
+                v-if="col === 'o'"
+                class="symbol symbol-o"
+                aria-label="O"
+                role="img"
+                viewBox="0 0 128 128">
+                <path d="M64,16A48,48 0 1,0 64,112A48,48 0 1,0 64,16"></path>
+              </svg>
+            </transition>
+          </button>
+        </div>
       </div>
+      <button class="replay" v-on:click="init()">
+        Rejouer
+      </button>
     </div>
   </div>
 </template>
@@ -39,15 +105,19 @@
 <script>
 import Vue from 'vue'
 
+
 export default {
   name: 'app',
   data () {
     return {
-      isPlayer: null,
+      o: 0,
+      x: 0,
+      turn: null,
+      hasBot: true,
       isFinish: false,
       showResult: false,
       isWait: false,
-      nbCase: 4,
+      nbCase: 3,
       values: null,
     }
   },
@@ -55,9 +125,11 @@ export default {
     isDisabled (row, col) {
       return this.isWait || this.values[row][col] !== null || this.isFinish
     },
+    changeTurn () {
+      this.turn = this.turn === 1 ? 2 : 1
+    },
     play (row, col) {
-      this.values[row][col] = this.isPlayer ? 'x' : 'o'
-      Vue.set(this.values[row], col, this.values[row][col])
+      Vue.set(this.values[row], col, this.turn === 1 ? 'x' : 'o')
       this.checkFinish()
     },
     playerPlay (row, col) {
@@ -68,8 +140,8 @@ export default {
 
       if (this.isFinish) return
 
-      this.isPlayer = !this.isPlayer
-      if (!this.isPlayer) {
+      this.changeTurn()
+      if (this.hasBot) {
         this.isWait = setTimeout(() => {
           this.playBot()
           this.isWait = null
@@ -91,10 +163,17 @@ export default {
 
       if (this.isFinish) return
 
-      this.isPlayer = !this.isPlayer
+      this.changeTurn()
     },
-    finish (result = null) {
-      this.isWinner = result
+    finish (result) {
+      if (typeof result === 'undefined') {
+        result = this.turn
+        if (result === 1)
+          this.x++
+        else
+          this.o++
+      }
+      this.result = result
       this.isFinish = true
       setTimeout(() => this.showResult = true, 2000)
     },
@@ -106,7 +185,7 @@ export default {
           values.push(this.values[i][j])
         }
         if (this.isEgual(values))
-          return this.finish(this.isPlayer)
+          return this.finish()
       }
 
       for (let i = 0; i < this.nbCase; i++) {
@@ -115,7 +194,7 @@ export default {
           values.push(this.values[j][i])
         }
         if (this.isEgual(values))
-          return this.finish(this.isPlayer)
+          return this.finish()
       }
 
       values = []
@@ -123,15 +202,15 @@ export default {
         values.push(this.values[i][i])
 
       if (this.isEgual(values))
-        return this.finish(this.isPlayer)
+        return this.finish()
 
       values = []
       for (let i = 0; i < this.nbCase; i++)
         values.push(this.values[i][(this.nbCase - 1) - i])
 
       if (this.isEgual(values))
-        return this.finish(this.isPlayer)
-        
+        return this.finish()
+
       let values = []
       this.values.forEach(row => {
         row.forEach(col => {
@@ -149,7 +228,7 @@ export default {
       return data.every(value => value === 'o')
     },
     init () {
-      this.isPlayer = true
+      this.turn = 1
       this.isFinish = false
       this.showResult = false
       this.values = []
@@ -174,7 +253,7 @@ body {
   font-family: sans-serif;
 }
 
-#app, .container, .row, .col {
+#app, .container, .head, .game, .result, .row, .col {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -185,21 +264,62 @@ body {
 #app {
   width: 100vw;
   height: 100vh;
-  background-color: #4CAF50;
+  background-color: #26A69A;
+}
+
+button {
+  outline: none;
 }
 
 .container {
-  width: 600px;
-  height: 600px;
   flex-direction: column;
 }
 
-.result {
+.symbol {
+  stroke-width: 12;
+  fill: transparent;
+  stroke: #000;
+  width: 200px;
+}
+
+.symbol-o {
+  stroke: #fff;
+}
+
+.head {
+  margin-bottom: 2rem;
+  width: 100%;
+  justify-content: flex-start;
+}
+
+.head button {
+  cursor: pointer;
+  border: none;
+  background-color: #B2DFDB;
+  height: 40px;
+  margin-left: 8px;
+  border-radius: 8px;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: rgba(76, 175, 80, 0.5);
+}
+
+.head .nb-win {
+  font-size: 1.8rem;
+  margin-left: 2rem;
+  color: #9e9e9e;
+}
+
+.head .play-bot {
+  flex: 1;
+  text-transform: uppercase;
+  font-weight: 600;
+  color: #9e9fa1;
+  margin-left: 0;
+}
+
+.head .symbol {
+  width: 24px;
 }
 
 .result div {
@@ -209,12 +329,24 @@ body {
   font-weight: 600;
 }
 
-.result button {
-  border: none;
-  padding: 0.8rem 1.6rem;
-  font-size: 2rem;
-  border-radius: 8px;
+.result, .game {
+  width: 600px;
+  height: 600px;
+  flex-direction: column;
+}
+
+.replay {
   cursor: pointer;
+  width: 100%;
+  border: none;
+  margin-top: 2rem;
+  padding: 1rem;
+  text-transform: uppercase;
+  font-weight: 800;
+  color: #9E9E9E;
+  border-radius: 8px;
+  background-color: #FAFAFA;
+  font-size: 1rem;
 }
 
 .row, .col {
@@ -228,11 +360,9 @@ body {
 .col,
 .col[disabled] {
   cursor: pointer;
-  border: 2px solid #000;
+  border: 4px solid #00897B;
   height: 100%;
-  font-size: 6rem;
   outline: none;
-  color: #000;
 }
 
 .col[disabled] {
@@ -255,7 +385,32 @@ body {
   border-right: none;
 }
 
-.col.col-o {
-  color: #fff;
+.col .symbol {
+  width: 80%;
+}
+
+.symbol-x-enter-active,
+.symbol-o-enter-active {
+  transition: stroke-dashoffset 0.225s cubic-bezier(0.4,0,0.2,1);
+}
+
+.symbol-x-enter {
+  stroke-dasharray: 135.764;
+  stroke-dashoffset: 135.764;
+}
+
+.symbol-x-enter-to {
+  stroke-dasharray: 135.764;
+  stroke-dashoffset: 0;
+}
+
+.symbol-o-enter {
+  stroke-dasharray: 301.635;
+  stroke-dashoffset: 301.635;
+}
+
+.symbol-o-enter-to {
+  stroke-dasharray: 301.635;
+  stroke-dashoffset: 0;
 }
 </style>
